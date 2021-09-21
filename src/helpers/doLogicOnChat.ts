a0642cc1f256acd0389454d40161280dbc0388be
janic0 <prodjnc@gmail.com>
2021-09-21 07:12:47 +0200

    fixed issue where sport lesson would not be called because of how promises work

--- src/helpers/doLogicOnChat.ts
+++ src/helpers/doLogicOnChat.ts
@@ -2,7 +2,7 @@ import { Chat } from "@prisma/client";
 import getCurrentLesson from "../interactions/getCurrentLesson";
 import getNextLesson from "../interactions/getNextLesson";
 import sendMessages from "./sendInfos";
-import data from "../davinci-data"
+import data from "../davinci-data";
 import findSpecificLesson from "../interactions/findSpecificLesson";
 
 const doLogicOnChat = (chat: Chat, now: Date, customCalled: boolean) => {
@@ -24,123 +24,128 @@ const doLogicOnChat = (chat: Chat, now: Date, customCalled: boolean) => {
 		};
 		getCurrentLesson(c, now).then((currentLesson) => {
 			getNextLesson(c, now).then((nextLesson) => {
-				if (currentLesson) {
-					if (nextLesson) {
-						const lessonStart = new Date(
-							now.getUTCFullYear(),
-							now.getUTCMonth(),
-							now.getUTCDate(),
-							parseInt(nextLesson.startTime[0] + nextLesson.startTime[1]) - 2,
-							parseInt(nextLesson.startTime[2] + nextLesson.startTime[3])
-						);
-						if (currentLesson.courseTitle === nextLesson.courseTitle) {
+				findSpecificLesson(c, "SPO", now).then((sportLesson) => {
+					if (currentLesson) {
+						if (nextLesson) {
+							const lessonStart = new Date(
+								now.getUTCFullYear(),
+								now.getUTCMonth(),
+								now.getUTCDate(),
+								parseInt(nextLesson.startTime[0] + nextLesson.startTime[1]) - 2,
+								parseInt(nextLesson.startTime[2] + nextLesson.startTime[3])
+							);
+							if (currentLesson.courseTitle === nextLesson.courseTitle) {
+								add(
+									"same lesson as before (" +
+										(data.subjects as any)[nextLesson.subjectCode] +
+										")"
+								);
+							} else {
+								add(
+									"next lesson: " +
+										(data.subjects as any)[nextLesson.subjectCode] +
+										" with " +
+										(data.teachers as any)[nextLesson.teacherCodes[0]]
+								);
+							}
+							if (currentLesson.roomCodes[0] === nextLesson.roomCodes[0]) {
+								add("same room (" + currentLesson.roomCodes[0] + ")");
+							} else {
+								add("move to room " + nextLesson.roomCodes[0]);
+							}
 							add(
-								"same lesson as before (" +
-									(data.subjects as any)[nextLesson.subjectCode] +
+								"starting in " +
+									Math.ceil(
+										(lessonStart.getTime() - now.getTime()) / 1000 / 60
+									).toString() +
+									" minutes (" +
+									(lessonStart.getHours() + 2).toString() +
+									":" +
+									lessonStart.getMinutes().toString() +
 									")"
 							);
 						} else {
+							if (sportLesson) {
+								add("Don't forget to take your turnsack home");
+							}
 							add(
-								"next lesson: " +
-									(data.subjects as any)[nextLesson.subjectCode] +
-									" with " +
-									(data.teachers as any)[nextLesson.teacherCodes[0]]
+								"lesson ending in " +
+									Math.ceil(
+										(new Date(
+											now.getUTCFullYear(),
+											now.getUTCMonth(),
+											now.getUTCDate(),
+											parseInt(
+												currentLesson.endTime[0] + currentLesson.endTime[1]
+											) - 2,
+											parseInt(
+												currentLesson.endTime[2] + currentLesson.endTime[3]
+											)
+										).getTime() -
+											new Date().getTime()) /
+											1000 /
+											60
+									).toString() +
+									" minutes, no more lesson"
 							);
 						}
-						if (currentLesson.roomCodes[0] === nextLesson.roomCodes[0]) {
-							add("same room (" + currentLesson.roomCodes[0] + ")");
-						} else {
-							add("move to room " + nextLesson.roomCodes[0]);
-						}
+					} else if (!currentLesson && nextLesson && now.getHours() + 2 < 8) {
+						add("first lesson:");
+						add((data.subjects as any)[nextLesson.subjectCode]);
+						add(nextLesson.roomCodes[0]);
+						add(
+							"with teacher " +
+								(data.teachers as any)[nextLesson.teacherCodes[0]]
+						);
 						add(
 							"starting in " +
-								Math.ceil(
-									(lessonStart.getTime() - now.getTime()) / 1000 / 60
-								).toString() +
-								" minutes (" +
-								(lessonStart.getHours() + 2).toString() +
-								":" +
-								lessonStart.getMinutes().toString() +
-								")"
-						);
-					} else {
-						findSpecificLesson(c, "SPO", now).then((a) => {
-								if (a) {
-									if (a.found) {
-										add("Don't forget your turnsack")
-									}
-								}
-						})
-						add(
-							"lesson ending in " +
 								Math.ceil(
 									(new Date(
 										now.getUTCFullYear(),
 										now.getUTCMonth(),
 										now.getUTCDate(),
 										parseInt(
-											currentLesson.endTime[0] + currentLesson.endTime[1]
+											nextLesson.startTime[0] + nextLesson.startTime[1]
 										) - 2,
-										parseInt(
-											currentLesson.endTime[2] + currentLesson.endTime[3]
-										)
+										parseInt(nextLesson.startTime[2] + nextLesson.startTime[3])
 									).getTime() -
 										new Date().getTime()) /
 										1000 /
 										60
 								).toString() +
-								" minutes, no more lesson"
+								" minutes"
 						);
+						if (sportLesson.found) {
+							add("You have sport today");
+						}
+					} else if (!currentLesson && nextLesson && now.getHours() + 2 > 7) {
+						add((data.subjects as any)[nextLesson.subjectCode]);
+						add(nextLesson.roomCodes[0]);
+						add((data.teachers as any)[nextLesson.teacherCodes[0]]);
+						add(
+							"in " +
+								Math.ceil(
+									(new Date(
+										now.getFullYear(),
+										now.getMonth(),
+										now.getDate(),
+										parseInt(
+											nextLesson.startTime[0] + nextLesson.startTime[1]
+										) - 2,
+										parseInt(nextLesson.startTime[2] + nextLesson.startTime[3])
+									).getTime() -
+										new Date().getTime()) /
+										1000 /
+										60
+								).toString() +
+								" minutes"
+						);
+					} else if (customCalled) {
+						add("no classes, enjoy your free-time");
 					}
-				} else if (!currentLesson && nextLesson && now.getHours() + 2 < 8) {
-					add("first lesson:");
-					add((data.subjects as any)[nextLesson.subjectCode]);
-					add("in room " + nextLesson.roomCodes[0]);
-					add("with teacher " + (data.teachers as any)[nextLesson.teacherCodes[0]]);
-					add(
-						"starting in " +
-							Math.ceil(
-								(new Date(
-									now.getUTCFullYear(),
-									now.getUTCMonth(),
-									now.getUTCDate(),
-									parseInt(nextLesson.startTime[0] + nextLesson.startTime[1]) -
-										2,
-									parseInt(nextLesson.startTime[2] + nextLesson.startTime[3])
-								).getTime() -
-									new Date().getTime()) /
-									1000 /
-									60
-							).toString() +
-							" minutes"
-					);
-				} else if (!currentLesson && nextLesson && now.getHours() + 2 > 7) {
-					add("next lesson: ");
-					add((data.subjects as any)[nextLesson.subjectCode]);
-					add("in room " + nextLesson.roomCodes[0]);
-					add("with teacher " + (data.teachers as any)[nextLesson.teacherCodes[0]]);
-					add(
-						"starting in " +
-							Math.ceil(
-								(new Date(
-									now.getFullYear(),
-									now.getMonth(),
-									now.getDate(),
-									parseInt(nextLesson.startTime[0] + nextLesson.startTime[1]) -
-										2,
-									parseInt(nextLesson.startTime[2] + nextLesson.startTime[3])
-								).getTime() -
-									new Date().getTime()) /
-									1000 /
-									60
-							).toString() +
-							" minutes"
-					);
-				} else if (customCalled) {
-          add("no classes, enjoy your free-time")
-        }
+				});
 				if (chat.classes.length - 1 === classIndex) {
-					sendMessages(chat, overWriteInfos, useOverWriteArray, c, infos)
+					sendMessages(chat, overWriteInfos, useOverWriteArray, c, infos);
 				}
 			});
 		});
